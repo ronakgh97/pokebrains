9077b309762dfbebdc0648581f3002627b2a84e5
Rounak Gh <ronakgh97@gmail.com>
2025-12-11 13:26:17 +0530

    Add fetch_pokemon_info function and data structures for Pokémon details

---
+++ src/bin/test_fetch.rs
@@ -0,0 +1,37 @@
+use pokebrains::{Result, Colorize};
+use pokebrains::fetch_pokemon_info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let data = fetch_pokemon_info("Darkrai").await;
+    match data {
+        Ok(info) => {
+            println!("Fetched data for: {}", info.name.bold());
+            print!("Pokemon types: ");
+            for t in info.types.iter() {
+                print!("{} ", t.r#type.name.bright_white());
+            }
+            println!();
+            println!("Pokemon Stats");
+            println!("Height: {}\nWeight: {}", info.height, info.weight);
+            for stat in info.stats {
+                println!("  {}: {}", stat.stat.name, stat.base_stat);
+            }
+            println!("Pokemon Abilities:");
+            for ability in info.abilities {
+                println!("  {} (hidden: {})", ability.ability.name, ability.is_hidden);
+            }
+            println!("Pokemon Moves:");
+            for mv in info.moves.iter().take(4) {
+                println!("  {}", mv.r#move.name);
+            }
+
+            println!("Total Moves: {}", info.moves.len());
+        }
+        Err(e) => {
+            eprintln!("Error fetching data: {}", e);
+        }
+    }
+
+    Ok(())
+}
---
+++ src/pokeapi.rs
@@ -0,0 +1,60 @@
+use anyhow::Result;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct PokemonInfo {
+    pub id: i32,
+    pub name: String,
+    pub height: i32,
+    pub weight: i32,
+    pub types: Vec<PokemonTypeSlot>,
+    pub abilities: Vec<PokemonAbilitySlot>,
+    pub moves: Vec<PokemonMoveSlot>,
+    pub stats: Vec<PokemonStat>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PokemonTypeSlot {
+    pub r#type: NamedAPIResource,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PokemonAbilitySlot {
+    pub is_hidden: bool,
+    pub ability: NamedAPIResource,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PokemonMoveSlot {
+    pub r#move: NamedAPIResource,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PokemonStat {
+    pub base_stat: i32,
+    pub stat: NamedAPIResource,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NamedAPIResource {
+    pub name: String,
+}
+
+pub async fn fetch_pokemon_info(pokemon_name: &str) -> Result<PokemonInfo> {
+    let url = format!(
+        "https://pokeapi.co/api/v2/pokemon/{}",
+        pokemon_name.to_lowercase()
+    );
+    let response = reqwest::get(&url).await?;
+
+    if response.status().is_success() {
+        let pokemon: PokemonInfo = response.json().await?;
+        Ok(pokemon)
+    } else {
+        Err(anyhow::anyhow!(
+            "Failed to fetch data for {}: {}",
+            pokemon_name,
+            response.status()
+        ))
+    }
+}
