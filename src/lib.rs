018d434e85523e9359fd55266a1763be8f76566f
Rounak Gh <ronakgh97@gmail.com>
2025-12-11 13:26:07 +0530

    Add pokeapi module for fetching Pokémon information

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 mod ai;
 mod client;
 mod parser;
+mod pokeapi;
 mod tools;
 
 pub use anyhow::Result;
 pub use client::BattleClient;
 pub use colored::Colorize;
+
+pub use pokeapi::fetch_pokemon_info;
