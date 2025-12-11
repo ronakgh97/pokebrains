0aaac7bb258df3d59596bb11ddd48f3bba7e93e1
Rounak Gh <ronakgh97@gmail.com>
2025-12-11 08:32:20 +0530

    Add BattleAgent struct and implementation for AI model handling

---
+++ src/ai.rs
@@ -0,0 +1,63 @@
+use anyhow::Result;
+use rig::agent::Agent;
+use rig::client::CompletionClient;
+use rig::providers::openai;
+use rig::providers::openai::responses_api::ResponsesCompletionModel;
+
+#[allow(unused)]
+pub enum ModelType {
+    Local,
+    Cloud,
+}
+
+#[allow(unused)]
+pub struct BattleAgent {
+    user: String,
+    opponent: String,
+    model: String,
+    model_type: ModelType,
+}
+
+impl BattleAgent {
+    pub fn _new(user: &str, opponent: &str, model: &str, model_type: ModelType) -> Self {
+        BattleAgent {
+            user: user.to_string(),
+            opponent: opponent.to_string(),
+            model: model.to_string(),
+            model_type,
+        }
+    }
+
+    pub fn _build_agent(self, api_key: &str) -> Result<Agent<ResponsesCompletionModel>> {
+        let agent = match self.model_type {
+            ModelType::Cloud => {
+                let client: openai::Client = openai::Client::builder()
+                    .base_url("https://openrouter/api/v1")
+                    .api_key(api_key)
+                    .build()?;
+
+                client
+                    .agent(self.model)
+                    .preamble("")
+                    .context("")
+                    .temperature(0.9)
+                    .build()
+            }
+            ModelType::Local => {
+                let client: openai::Client = openai::Client::builder()
+                    .base_url("http://localhost:1234/v1")
+                    .api_key(api_key)
+                    .build()?;
+
+                client
+                    .agent(self.model)
+                    .preamble("")
+                    .context("")
+                    .temperature(0.8)
+                    .build()
+            }
+        };
+
+        Ok(agent)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+mod ai;
+mod client;
+mod parser;
+mod tools;
+
+pub use anyhow::Result;
+pub use client::BattleClient;
+pub use colored::Colorize;
