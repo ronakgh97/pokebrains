a3f97a68fa1e8ac6aaf9f77e92e8973fea043c3e
ronakgh97 <ronakgh97@gmail.com>
2025-12-16 22:04:39 +0530

    Update test_fetch.rs to fetch data for Latios and enhance output formatting

--- src/bin/test_fetch.rs
+++ src/bin/test_fetch.rs
@@ -3,30 +3,48 @@ use pokebrains::{Colorize, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let data = fetch_pokemon_info("Darkrai").await;
+    let data = fetch_pokemon_info("Latios").await;
     match data {
         Ok(info) => {
-            println!("Fetched data for: {}", info.name.bold());
-            print!("Pokemon types: ");
+            println!();
+            println!("Pokemon : {}", info.name.bright_white().bold());
+            print!("{}", "Types: ".bright_red().bold());
             for t in info.types.iter() {
-                print!("{} ", t.r#type.name.bright_white());
+                print!(" {} ", t.r#type.name.bright_red());
             }
             println!();
-            println!("Pokemon Stats");
-            println!("Height: {}\nWeight: {}", info.height, info.weight);
+            println!("{}", "Stats: ".bright_blue().bold());
             for stat in info.stats {
-                println!("  {}: {}", stat.stat.name, stat.base_stat);
+                println!(
+                    " {} : {}",
+                    stat.stat.name.bright_blue(),
+                    stat.base_stat.to_string().bright_blue()
+                );
             }
-            println!("Pokemon Abilities:");
+            println!(
+                " Height: {}\n Weight: {}",
+                info.height.to_string().bright_magenta(),
+                info.weight.to_string().bright_magenta()
+            );
+            println!();
+            println!("{}", "Abilities: ".on_bright_green().bold());
             for ability in info.abilities {
-                println!("  {} (hidden: {})", ability.ability.name, ability.is_hidden);
+                println!(
+                    " {} (hidden: {})",
+                    ability.ability.name.bright_green(),
+                    ability.is_hidden.to_string().bright_green()
+                );
             }
-            println!("Pokemon Moves:");
+            println!();
+            println!("{}", "Moves: ".bright_red().bold());
             for mv in info.moves.iter().take(4) {
-                println!("  {}", mv.r#move.name);
+                println!(" {}", mv.r#move.name.to_string().red());
             }
 
-            println!("Total Moves: {}", info.moves.len());
+            println!(
+                "Total Moves: {}",
+                info.moves.len().to_string().bright_yellow()
+            );
         }
         Err(e) => {
             eprintln!("Error fetching data: {}", e);
