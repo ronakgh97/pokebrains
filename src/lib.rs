7df795864974b3a44b37b45a41cdc238d4d7bcce
ronakgh97 <ronakgh97@gmail.com>
2025-12-19 17:56:47 +0530

    Refactor module naming: Rename ai module to agent and update imports accordingly

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-mod ai;
+mod agent;
 mod client;
 mod parser;
 mod pokeapi;
 
-pub use ai::{BattleAgent, ModelType};
+pub use agent::{BattleAgent, ModelType};
 pub use anyhow::Result;
 pub use client::BattleClient;
 pub use colored::Colorize;
+pub use parser::logs::{BattleEvents, Token};
 pub use parser::team::Team;
 pub use pokeapi::{display, fetch_pokemon_info};
